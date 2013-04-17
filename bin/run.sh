22254303b8f661edb245958f75a9f38a2b641c6f
Joao Lima <Joao.Lima@imag.fr>
2013-04-17 13:18:07 -0300

    [sparselu] bugio config.

--- bin/run.sh
+++ bin/run.sh
@@ -3,8 +3,9 @@
 export LD_LIBRARY_PATH=$HOME/install/xkaapi/default/lib:$LD_LIBRARY_PATH
 
 function run_test {
-    export KAAPI_CPUSET="0"
-    export KAAPI_GPUSET="0~1"
+    # test case for Bugio
+    export KAAPI_CPUSET="0:6"
+    export KAAPI_GPUSET="0~7"
 
 #    export KAAPI_CPUSET="0,5,6,11"
 #    export KAAPI_GPUSET="0~1,1~2,2~3,3~4,4~7,5~8,6~9,7~10"
@@ -23,7 +24,6 @@ function run_test {
 #
 #    export KAAPI_PUSH_AFFINITY="writer"
 #    export KAAPI_PUSH_AFFINITY="heft"
-#    export KAAPI_HEFT_CALIBRATE=1
 #    export KAAPI_STEAL_AFFINITY="writer"
 #    export KAAPI_PUSH_AFFINITY="locality"
 #    export KAAPI_STEAL_AFFINITY="locality"
@@ -31,9 +31,19 @@ function run_test {
 
 #    execfile="./strassen.gcc.kaapi"
     execfile="./sparselu.gcc.kaapi"
+
     export KAAPI_CUDA_WINDOW_SIZE=2
+
+    nsizes="64 128"
+    nblocks="32"
 #   KAAPI_STACKSIZE_MASTER=536870912 gdb $execfile
-    KAAPI_STACKSIZE_MASTER=536870912 $execfile -n 25 -m 25
+    for size in $nsizes
+    do
+      for nb in $nblocks
+      do
+	KAAPI_STACKSIZE_MASTER=536870912 $execfile -n $size -m $nb
+      done
+    done
 }
 
 run_test
